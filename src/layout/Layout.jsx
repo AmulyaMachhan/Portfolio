1e86393062c523391eac5c022ea985e07b76d625
AmulyaMachhan <machhanamulya@gmail.com>
2025-12-10 13:55:23 +0530

    Updated

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,14 +1,18 @@
 import { Outlet } from "react-router";
 import Navbar from "./Navbar";
+import Footer from "./Footer";
 
 function Layout() {
   return (
-    <div className="flex flex-col min-h-screen relative bg-black">
-      <Navbar />
-      <main className="flex-grow">
-        <Outlet />
-      </main>
-    </div>
+    <>
+      <div className="flex flex-col min-h-screen relative bg-black">
+        <Navbar />
+        <main className="flex-grow">
+          <Outlet />
+        </main>
+      </div>
+      <Footer />
+    </>
   );
 }
 
