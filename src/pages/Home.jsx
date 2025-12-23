0779e2df71dba4feb7a0246c7d9d74f22b9716e8
AmulyaMachhan <machhanamulya@gmail.com>
2025-12-23 13:55:23 +0530

    Updated Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,7 @@
-import { Suspense } from "react";
 import Hero from "../components/Hero";
 
 function Home() {
-  return (
-    <Suspense
-      fallback={
-        <div className="w-full h-screen flex items-center justify-center bg-black text-white text-xl">
-          Initializing Hyperspeed...
-        </div>
-      }
-    >
-      <Hero />
-    </Suspense>
-  );
+  return <Hero />;
 }
 
 export default Home;
