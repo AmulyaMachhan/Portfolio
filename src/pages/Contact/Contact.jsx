a2f610658071a3c2ab8d1fc320829f72ef33c515
AmulyaMachhan <machhanamulya@gmail.com>
2026-01-02 18:25:52 +0530

    Updated UI

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -4,12 +4,12 @@ import ContactDetails from "./ContactDetails";
 
 export default function Contact() {
   return (
-    <section className="text-gray-100 ">
+    <section className="text-gray-100 bg-gradient-to-b from-grey-900 via-blue-900 to-grey-600">
       {/* Section 1: Caption + Banners */}
       <ContactBanner />
 
       {/* Section 2: Contact Info + Form */}
-      <div className="flex flex-col md:flex-row justify-evenly gap-10 py-25 px-4 lg:px-12">
+      <div className="flex flex-col md:flex-row justify-evenly gap-10 py-10 px-4 lg:px-12">
         <div className="[contain:layout] absolute inset-0 bg-[radial-gradient(circle_at_top_left,rgba(236,72,153,0.08),transparent_70%)]"></div>
 
         {/* LEFT SIDE */}
